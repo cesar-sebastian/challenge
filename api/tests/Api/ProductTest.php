84d0cf48228f12bc96ab195a01ec1c1a53d8aa2b
CSC <cesarcarrazana@gmail.com>
2021-05-18 09:46:23 -0300

    messengers test

--- api/tests/Api/ProductTest.php
+++ api/tests/Api/ProductTest.php
@@ -6,11 +6,12 @@ use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
 use App\Entity\Product;
 use Doctrine\ORM\EntityManager;
 use Hautelook\AliceBundle\PhpUnit\RefreshDatabaseTrait;
+use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\Client;
-use Symfony\Bundle\FrameworkBundle\Console\Application;
-use Symfony\Component\Console\Input\ArrayInput;
-use Symfony\Component\Console\Output\BufferedOutput;
+use Symfony\Component\Messenger\EventListener\StopWorkerOnMessageLimitListener;
+use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Messenger\Worker;
 
 class ProductTest extends ApiTestCase
 {
@@ -41,27 +42,23 @@ class ProductTest extends ApiTestCase
 
         $this->assertResponseStatusCodeSame(201);
 
+        //$this->consume('async_priority_high', 6);
+
         $products = $this->em->getRepository(Product::class)->findAll();
         $this->assertCount(6, $products);
     }
 
+    public function consume($transport, $number)
+    {
+        $transport = static::$container->get(sprintf('messenger.transport.%s', $transport));
+        $bus = static::$container->get(MessageBusInterface::class);
+        $eventDispatcher = new EventDispatcher();
+        $eventDispatcher->addSubscriber(new StopWorkerOnMessageLimitListener($number));
 
-//    public function consume($queue)
-//    {
-//        $kernel = static::createKernel();
-//        $application = new Application($kernel);
-//        $application->setAutoExit(false);
-//
-//        $input = new ArrayInput([
-//            'command' => 'messenger:consume '
-//        ]);
-//
-//        // You can use NullOutput() if you don't need the output
-//        $output = new BufferedOutput();
-//        $application->run($input, $output);
-//
-//        $content = $output->fetch();
-//
-//    }
+        $worker = new Worker([$transport], $bus, $eventDispatcher);
+        $worker->run([
+            'sleep' => 0,
+        ]);
+    }
 
 }
